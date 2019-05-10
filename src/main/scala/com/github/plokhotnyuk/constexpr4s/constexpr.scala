a29b9fcff59699ef40fb031df9f7cb9994082d7e
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-05-10 13:08:08 +0200

    Code clean up

--- src/main/scala/com/github/plokhotnyuk/constexpr4s/constexpr.scala
+++ src/main/scala/com/github/plokhotnyuk/constexpr4s/constexpr.scala
@@ -10,27 +10,27 @@ object constexpr {
     def apply[A: c.WeakTypeTag](c: blackbox.Context)(expr: c.Expr[A]): c.Expr[A] = {
       import c.universe._
 
-      def evalAs[B]: B = c.eval[B](c.Expr[B](c.untypecheck(expr.tree)))
+      implicit def lift: c.universe.Liftable[A] = Liftable[A]({
+        case x: Byte => q"$x"
+        case x: Boolean => q"$x"
+        case x: Short => q"$x"
+        case x: Char => q"$x"
+        case x: Int => q"$x"
+        case x: Float => q"$x"
+        case x: Long => q"$x"
+        case x: Double => q"$x"
+        case x: Array[Byte] => q"$x"
+        case x: Array[Boolean] => q"$x"
+        case x: Array[Short] => q"$x"
+        case x: Array[Char] => q"$x"
+        case x: Array[Int] => q"$x"
+        case x: Array[Float] => q"$x"
+        case x: Array[Long] => q"$x"
+        case x: Array[Double] => q"$x"
+        case _ => c.abort(c.enclosingPosition, s"Unsupported type of expression: '${weakTypeOf[A].dealias}'")
+      })
 
-      val resultTpe = weakTypeOf[A].dealias
-      val result: Tree =
-        if (resultTpe =:= definitions.ByteTpe) q"${evalAs[Byte]}"
-        else if (resultTpe =:= definitions.BooleanTpe) q"${evalAs[Boolean]}"
-        else if (resultTpe =:= definitions.ShortTpe) q"${evalAs[Short]}"
-        else if (resultTpe =:= definitions.CharTpe) q"${evalAs[Char]}"
-        else if (resultTpe =:= definitions.IntTpe) q"${evalAs[Int]}"
-        else if (resultTpe =:= definitions.FloatTpe) q"${evalAs[Float]}"
-        else if (resultTpe =:= definitions.LongTpe) q"${evalAs[Long]}"
-        else if (resultTpe =:= definitions.DoubleTpe) q"${evalAs[Double]}"
-        else if (resultTpe =:= typeOf[Array[Byte]]) q"${evalAs[Array[Byte]]}"
-        else if (resultTpe =:= typeOf[Array[Boolean]]) q"${evalAs[Array[Boolean]]}"
-        else if (resultTpe =:= typeOf[Array[Short]]) q"${evalAs[Array[Short]]}"
-        else if (resultTpe =:= typeOf[Array[Char]]) q"${evalAs[Array[Char]]}"
-        else if (resultTpe =:= typeOf[Array[Int]]) q"${evalAs[Array[Int]]}"
-        else if (resultTpe =:= typeOf[Array[Float]]) q"${evalAs[Array[Float]]}"
-        else if (resultTpe =:= typeOf[Array[Long]]) q"${evalAs[Array[Long]]}"
-        else if (resultTpe =:= typeOf[Array[Double]]) q"${evalAs[Array[Double]]}"
-        else c.abort(c.enclosingPosition, s"Unsupported type of expression: '$resultTpe'")
+      val result: Tree = q"${c.eval[A](c.Expr[A](c.untypecheck(expr.tree.duplicate)))}"
       if (c.settings.contains("print-expr-results")) {
         c.info(c.enclosingPosition, s"Expression result:\n${showCode(result)}", force = true)
       }
